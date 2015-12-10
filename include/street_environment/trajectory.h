e241ebebbb68e5b1a4d0fe12282796855cf67c62
phibedy <philipp.schmette@gmx.de>
2015-12-10 16:47:38 +0100

    added trajectory

---
+++ include/street_environment/trajectory.h
@@ -0,0 +1,38 @@
+#ifndef STREET_ENVIRONMENT_TRAJECTORY_H
+#define STREET_ENVIRONMENT_TRAJECTORY_H
+#include "lms/math/polyline.h"
+
+namespace street_environment {
+    class Trajectory: public lms::math::polyLine2f{
+        struct RoadChange{
+            RoadChange(){}
+            RoadChange(int index, bool left):changeRoadIndex(index),changeToLeft(left){}
+            int changeRoadIndex;
+            bool changeToLeft;
+        };
+    public:
+        std::vector<RoadChange> changes;
+        virtual void lmsSerialize(std::ostream &os) const override{
+            (void)os;
+            //TODO
+        }
+        virtual void lmsDeserialize(std::istream &is) override{
+            (void)is;
+            //TODO
+        }
+
+        virtual bool isSubType(size_t hashcode) const override{
+            return hashcode == typeid(lms::math::polyLine2f).hash_code();
+        }
+        void addChange(int index, bool left){
+            changes.push_back(RoadChange(index,left));
+        }
+    };
+
+    struct TrajectoryPoint{
+        lms::math::vertex2f position;
+        lms::math::vertex2f directory;
+    };
+
+}
+#endif //STREET_ENVIRONMENT_TRAJECTORY_H

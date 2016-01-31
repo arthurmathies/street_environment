a5f4473145e722654903eac73fec33bfa2c063a0
phibedy <philipp.schmette@gmx.de>
2016-01-31 03:39:23 +0100

    refactor Trajectory

--- include/street_environment/trajectory.h
+++ include/street_environment/trajectory.h
@@ -8,52 +8,50 @@
 #endif
 
 namespace street_environment {
-    class Trajectory: public lms::math::polyLine2f{
-    public:
-        struct RoadChange{
-            RoadChange(){}
-            RoadChange(int index, bool left):changeRoadIndex(index),changeToLeft(left){}
-            int changeRoadIndex;
-            bool changeToLeft;
-
-            template<class Archive>
-            void serialize(Archive &archive) {
-                archive(changeRoadIndex, changeToLeft);
-            }
-        };
-        std::vector<RoadChange> changes;
-        lms::math::polyLine2f viewDirs;
-
-        template<class Archive>
-        void serialize(Archive &archive) {
-            archive(cereal::base_class<lms::math::polyLine2f>(this), changes);
-        }
-
-        CEREAL_SERIALIZATION()
-
-        virtual bool isSubType(size_t hashcode) const override{
-            return hashcode == typeid(lms::math::polyLine2f).hash_code();
-        }
-        void addChange(int index, bool left){
-            changes.push_back(RoadChange(index,left));
-        }
-    };
 
     struct TrajectoryPoint:public lms::Serializable{
         lms::math::vertex2f position;
         lms::math::vertex2f directory;
         float velocity;
-        TrajectoryPoint():position(0,0),directory(1,0),velocity(0){
+        float distanceToMiddleLane;
+        /**
+         * @brief isRight
+         * @return true if the TrajectoryPoint is on the left side of the middle of the road
+         */
+        bool isRight(){
+            return distanceToMiddleLane > 0;
+        }
+
+        TrajectoryPoint():position(0,0),directory(1,0),velocity(0),distanceToMiddleLane(0){
 
         }
-#ifdef USE_CEREAL
+        TrajectoryPoint(lms::math::vertex2f pos,lms::math::vertex2f viewDir, float velocity_, float distanceToMiddleLane_):position(pos),directory(viewDir),velocity(velocity_),distanceToMiddleLane(distanceToMiddleLane_){
+
+        }
+    #ifdef USE_CEREAL
         CEREAL_SERIALIZATION()
 
         template<class Archive>
         void serialize(Archive &ar) {
-            ar(position, directory, velocity);
+            ar(position, directory, velocity,distanceToMiddleLane);
         }
+    #endif
+    };
+
+    class Trajectory: public std::vector<TrajectoryPoint>, public lms::Inheritance/* TODO , public lms::Serializable*/{
+    public:
+        template<class Archive>
+        void serialize(Archive &archive) {
+            archive(std::vector<TrajectoryPoint>(this));
+        }
+/* TODO
+#ifdef USE_CEREAL
+        CEREAL_SERIALIZATION()
 #endif
+*/
+        virtual bool isSubType(size_t hashcode) const override{
+            return hashcode == typeid(std::vector<TrajectoryPoint>).hash_code();
+        }
     };
 
 }

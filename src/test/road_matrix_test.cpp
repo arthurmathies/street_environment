c7cf9ca1dab3df7c3f54a72029290efdc5576da1
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-15 10:43:05 +0100

    Fix road_matrix_test

--- src/test/road_matrix_test.cpp
+++ src/test/road_matrix_test.cpp
@@ -1,28 +1,26 @@
 #include "street_environment/road_matrix/road_matrix.h"
 
-#include <vector>
-
-#include "gtest/gtest.h"
 #include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include "lms/math/polyline.h"
 
 using lms::math::polyLine2f;
 using lms::math::vertex2f;
 
 namespace street_environment {
+
 TEST(RoadMatrix, aroundLine) {
     polyLine2f straightLine;
     int lineLength = 20;
-    vertex2f pointOnLine;
     for (int i = 0; i < lineLength; i++) {
-        pointOnLine.x = i * 0.1;
-        pointOnLine.y = 0;
-        straightLine.points().push_back(pointOnLine);
+        straightLine.points().push_back(vertex2f(i * 0.1, 0.1));
     }
 
     RoadMatrix roadMatrix;
     float laneWidth = 0.4;
     int cellsPerLane = 4;
-    roadMatrix.aroundLine(straightLine, laneWidth, cellsPerLane);
+    float cellLength = 0.1;
+    roadMatrix.aroundLine(straightLine, laneWidth, cellsPerLane, cellLength);
 }
-}
+
+}  // namespace street_environment

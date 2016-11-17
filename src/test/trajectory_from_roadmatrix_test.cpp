d9dbe0d6b0c32557d49abe9f6e1f67655610d7b4
Arthur Mathies <arthur.mathies@googlemail.com>
2016-11-17 10:39:39 +0100

    Add some simple tests for blocked road

--- src/test/trajectory_from_roadmatrix_test.cpp
+++ src/test/trajectory_from_roadmatrix_test.cpp
@@ -14,19 +14,37 @@ using lms::math::vertex2f;
 
 namespace street_environment {
 
-TEST(TrajectoryFromRoadmatrix, valueFunction) {
-    polyLine2f straightLine;
-    const int lineLength = 20;
-    for (int i = 0; i < lineLength; i++) {
-        straightLine.points().push_back(vertex2f(i * 0.1, 0.2));
+class TrajectoryFromRoadMatrixTest : public testing::Test {
+   public:
+    TrajectoryFromRoadMatrixTest() : lineLength(20), laneWidth(0.4),
+        cellsPerLane(4), cellLength(0.1), carWidthMeter(0.2),
+        obstacleClearanceMeter(0.5) {
+        polyLine2f straightLine;
+        const int lineLength = 20;
+        for (int i = 0; i < lineLength; i++) {
+            straightLine.points().push_back(vertex2f(i * 0.1, 0.2));
+        }
+
+        roadMatrix.aroundLine(straightLine, laneWidth, cellsPerLane,
+                              cellLength);
+
+        trajectory_creator.setCarWidthMeter(carWidthMeter);
+        trajectory_creator.setObstacleClearanceMeter(obstacleClearanceMeter);
+        trajectory_creator.calculateCycleConstants(roadMatrix);
     }
 
     RoadMatrix roadMatrix;
-    const float laneWidth = 0.4;
-    const int cellsPerLane = 4;
-    const float cellLength = 0.1;
-    roadMatrix.aroundLine(straightLine, laneWidth, cellsPerLane, cellLength);
+    TrajectoryFromRoadmatrixImpl trajectory_creator;
 
+    const int lineLength;
+    const float laneWidth;
+    const int cellsPerLane;
+    const float cellLength;
+    const float carWidthMeter;
+    const float obstacleClearanceMeter;
+};
+
+TEST_F(TrajectoryFromRoadMatrixTest, valueFunction) {
     std::vector<EnvironmentObjectPtr> obstacles;
 
     street_environment::ObstaclePtr obst1(new street_environment::Obstacle());
@@ -35,16 +53,37 @@ TEST(TrajectoryFromRoadmatrix, valueFunction) {
 
     roadMatrix.markEnvironmentObjects(obstacles);
 
-    TrajectoryFromRoadmatrixImpl trajectory_creator;
-    trajectory_creator.setCarWidthMeter(0.2);
-    trajectory_creator.setObstacleClearanceMeter(0.5);
-    trajectory_creator.calculateCycleConstants(roadMatrix);
-
     EXPECT_EQ(
         trajectory_creator.valueFunction(roadMatrix.cell(0, 0), roadMatrix),
         12);
     EXPECT_EQ(
         trajectory_creator.valueFunction(roadMatrix.cell(0, 5), roadMatrix), 5);
+}
+
+TEST_F(TrajectoryFromRoadMatrixTest, getOptimalLanePieceTrajectoryBlockedRoad) {
+    std::vector<EnvironmentObjectPtr> obstacles;
+
+    street_environment::ObstaclePtr obst1(new street_environment::Obstacle());
+    obst1->addPoint(lms::math::vertex2f(0.7, -0.15));
+    obst1->addPoint(lms::math::vertex2f(0.7, -0.05));
+    obst1->addPoint(lms::math::vertex2f(0.7, 0.05));
+    obst1->addPoint(lms::math::vertex2f(0.7, 0.15));
+    obst1->addPoint(lms::math::vertex2f(0.7, 0.25));
+    obst1->addPoint(lms::math::vertex2f(0.7, 0.35));
+    obst1->addPoint(lms::math::vertex2f(0.7, 0.45));
+    obst1->addPoint(lms::math::vertex2f(0.7, 0.55));
+    obstacles.push_back(obst1);
+
+    roadMatrix.markEnvironmentObjects(obstacles);
+
+    std::unique_ptr<LanePieceMatrix> lanePieceMatrix =
+        trajectory_creator.createLanePieceMatrix(roadMatrix);
+
+    std::unique_ptr<LanePieceTrajectory> lanePieceTrajectory =
+        trajectory_creator.getOptimalLanePieceTrajectory(*lanePieceMatrix);
+
+    EXPECT_EQ(lanePieceTrajectory->size(), 2);
+    EXPECT_EQ(lanePieceTrajectory->back().stop, true);
 }
 
 }  // namespace street_environment

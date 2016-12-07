e6248035591d3b961ac09c0e50bdaac7ac226bb6
Arthur Mathies <arthur.mathies@googlemail.com>
2016-12-07 15:00:32 +0100

    First iteration of back view roadmatrix

--- src/roadmatrix.cpp
+++ src/roadmatrix.cpp
@@ -1,46 +1,37 @@
 #include "street_environment/roadmatrix.h"
 
+#include <algorithm>
+
 namespace street_environment {
 
-RoadMatrixCell RoadMatrix::initCell(int x, int y) const {
-    RoadMatrixCell c;
-    c.x = x;
-    c.y = y;
-    int arrayLength = m_length + 1;
-    c.points[0] = m_points[x + arrayLength * y];
-    c.points[1] = m_points[x + 1 + arrayLength * y];
-    c.points[2] = m_points[x + 1 + arrayLength * (y + 1)];
-    c.points[3] = m_points[x + arrayLength * (y + 1)];
-    return c;
-}
-
-void RoadMatrix::initCells() {
-    m_cells.clear();
-    m_cells.resize(length(), std::vector<RoadMatrixCell>(width()));
-    for (int x = 0; x < length(); x++) {
-        for (int y = 0; y < width(); y++) {
-            m_cells[x][y] = initCell(x, y);
-        }
+bool RoadMatrix::initialize(float laneWidth, int cellsPerLane, float cellLength,
+                            int maxTranslation) {
+    if (!m_initalized) {
+        m_cellsPerLane = cellsPerLane;
+        m_cellLength = cellLength;
+        m_cellWidth = static_cast<float>(laneWidth / m_cellsPerLane);
+        m_width = cellsPerLane * 2;
+        m_pWidth = m_width + 1;
+        m_maxTranslation = maxTranslation;
+        return true;
     }
+    return false;
 }
 
-void RoadMatrix::aroundLine(const lms::math::polyLine2f &line, float laneWidth,
-                            int cellsPerLane, float cellLength) {
-    m_cellWidth = laneWidth / cellsPerLane;
-    m_cellLength = cellLength;
+void RoadMatrix::aroundLine(const lms::math::polyLine2f& line,
+                            const lms::math::vertex2f& deltaPosition,
+                            float deltaRotation) {
+    if (size() > 0) {
+        if (deltaPosition.x >= m_cellLength) {
+            translate(deltaPosition);
+        }
+        rotate(deltaRotation);
+    }
     lms::math::polyLine2f scaledLine =
         line.getWithDistanceBetweenPoints(m_cellLength);
-    m_width = cellsPerLane * 2;
-    m_length = scaledLine.points().size() - 1;
-
-    m_points.clear();
-    for (int i = cellsPerLane; i >= -cellsPerLane; i--) {
-        lms::math::polyLine2f movedLine =
-            scaledLine.moveOrthogonal(m_cellWidth * i);
-        m_points.insert(std::end(m_points), std::begin(movedLine.points()),
-                        std::end(movedLine.points()));
+    if (scaledLine.points().size() >= 2) {
+        aroundLine(scaledLine);
     }
-
     initCells();
 }
 
@@ -56,6 +47,97 @@ bool RoadMatrix::findCell(const lms::math::vertex2f& v,
         }
     }
     return false;
+}
+
+void RoadMatrix::rotate(float deltaRotation) {
+    for (int x = 0; x < m_translation; x++) {
+        for (int y = 0; y < m_pWidth; y++) {
+            point(x, y) = point(x, y).rotate(deltaRotation);
+        }
+    }
+}
+
+void RoadMatrix::translate(const lms::math::vertex2f& deltaPosition) {
+    RoadMatrixCell deltaCell;
+    if (findCell(deltaPosition, &deltaCell)) {
+        int prevTranslation = m_translation;
+        int deltaX = deltaCell.x - prevTranslation;
+        m_translation = std::min(deltaX + prevTranslation, m_maxTranslation);
+        int firstColumn = deltaX - m_translation + prevTranslation;
+        for (int x = 0; x < m_translation; x++) {
+            for (int y = 0; y < m_pWidth; y++) {
+                point(x, y) = point(x + firstColumn, y) - deltaPosition;
+            }
+        }
+        m_points.resize(m_translation * m_pWidth);
+        m_length = m_translation - 1;
+        m_pLength = m_translation;
+    }
+}
+
+void RoadMatrix::aroundLine(const lms::math::polyLine2f& line) {
+    m_length = line.points().size() - 1 + m_translation;
+    m_pLength = m_length + 1;
+    m_points.resize(m_pLength * m_pWidth);
+    for (int dy = m_cellsPerLane; dy >= -m_cellsPerLane; dy--) {
+        lms::math::polyLine2f movedLine = line.moveOrthogonal(m_cellWidth * dy);
+        for (int x = m_translation, i = 0; x < m_pLength; x++, i++) {
+            int y = -(dy - m_cellsPerLane);
+            point(x, y) = movedLine.points().at(i);
+        }
+    }
+}
+
+RoadMatrixCell RoadMatrix::initCell(int x, int y) const {
+    RoadMatrixCell c;
+    c.x = x;
+    c.y = y;
+    c.points.at(0) = point(x, y);
+    c.points.at(1) = point(x + 1, y);
+    c.points.at(2) = point(x + 1, y + 1);
+    c.points.at(3) = point(x, y + 1);
+    return c;
+}
+
+void RoadMatrix::initCells() {
+    m_cells.resize(length() * width());
+    for (int x = 0; x < length(); x++) {
+        for (int y = 0; y < width(); y++) {
+            cell(x, y) = initCell(x, y);
+        }
+    }
+}
+
+const RoadMatrixCell& RoadMatrix::cell(int x, int y) const {
+    return m_cells.at(x * m_width + y);
+}
+RoadMatrixCell& RoadMatrix::cell(int x, int y) {
+    return m_cells.at(x * m_width + y);
+}
+
+const lms::math::vertex2f& RoadMatrix::point(int x, int y) const {
+    return m_points.at(x * m_pWidth + y);
+}
+lms::math::vertex2f& RoadMatrix::point(int x, int y) {
+    return m_points.at(x * m_pWidth + y);
+}
+
+std::ostream& operator<<(std::ostream& stream, const RoadMatrix& matrix) {
+    stream << "[\n";
+    for (int y = matrix.width() - 1; y >= 0; y--) {
+        stream << "[";
+        for (int x = 0; x < matrix.length(); x++) {
+            stream << "[";
+            for (int i = 0; i < 4; i++) {
+                stream << "(" << matrix.cell(x, y).points[i].x << ","
+                       << matrix.cell(x, y).points[i].y << ")";
+            }
+            stream << "]";
+        }
+        stream << "]\n";
+    }
+    stream << "]\n";
+    return stream;
 }
 
 }  // namespace street_environment
